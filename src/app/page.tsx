c18bed947299c7893c6215d10af44529a163e9ae
Shogo Kitada <contact@shogo0x2e.com>
2024-05-28 16:26:25 +0900

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
+import { CreatePost } from "~/components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
@@ -22,8 +22,8 @@ export default async function Home() {
           >
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+              Just the basics - Everything you need to know to set up your database and
+              authentication.
             </div>
           </Link>
           <Link
@@ -33,15 +33,12 @@ export default async function Home() {
           >
             <h3 className="text-2xl font-bold">Documentation →</h3>
             <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+              Learn more about Create T3 App, the libraries it uses, and how to deploy it.
             </div>
           </Link>
         </div>
         <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
+          <p className="text-2xl text-white">{hello ? hello.greeting : "Loading tRPC query..."}</p>
 
           <div className="flex flex-col items-center justify-center gap-4">
             <p className="text-center text-2xl text-white">
