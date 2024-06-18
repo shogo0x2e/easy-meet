d7741f8ad23f7ea2dac0d559b2d3d4b8fa46e9ed
Shogo Kitada <contact@shogo0x2e.com>
2024-06-18 14:52:00 +0900

    initialize with discord

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+  printWidth: 100,
+  trailingComma: "all",
+  bracketSameLine: false,
 };
 
 export default config;
